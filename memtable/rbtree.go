5ca4c9ffba8567647365c58278fe1297b167ab75
staf3333 <staf3333@gmail.com>
2026-03-12 19:50:30 -0700

    add insert and insert fix up algorithm to the Red Black Tree

--- memtable/rbtree.go
+++ memtable/rbtree.go
@@ -70,3 +70,89 @@ func (t *RBTree) rightRotate(node *node) {
 
 	node.parent = pivot
 }
+
+func (t *RBTree) Insert(key string, value string) {
+	newNode := &node{
+		key: key,
+		value: value,
+		color: red,
+		left: t.sentinel,
+		right: t.sentinel,
+	}
+
+	// handle case where this is the first node in the tree
+	if t.root == t.sentinel {
+		t.root = newNode
+		newNode.parent = t.sentinel
+		// root is always black
+		newNode.color = black
+		return
+	}
+	
+	// iterate through tree to find the proper location
+	curr := t.root
+	parent := t.root.parent
+
+	for curr != t.sentinel {
+		if (newNode.key == curr.key) {
+			curr.value = newNode.value
+			return
+		}
+
+		parent = curr
+		if (newNode.key < curr.key) {
+			curr = curr.left
+		} else {
+			curr = curr.right
+		}
+	}
+
+	if newNode.key < parent.key {
+		parent.left = newNode
+	} else {
+		parent.right = newNode
+	}
+	newNode.parent = parent
+	t.insertFixup(newNode)
+}
+
+func (t *RBTree) insertFixup(newNode *node) {
+	for newNode.parent.color == red {
+		parent := newNode.parent
+		grandparent := newNode.parent.parent
+		if parent == grandparent.left {
+			uncle := grandparent.right
+			if uncle.color == red {
+				parent.color = black
+				uncle.color = black
+				grandparent.color = red
+				newNode = grandparent
+			} else {
+				if newNode == parent.right {
+					t.leftRotate(parent)
+					newNode = parent
+				} 	
+				t.rightRotate(grandparent)
+				grandparent.color = red
+				newNode.parent.color = black
+			}
+		} else {
+			uncle := grandparent.left
+			if uncle.color == red {
+				parent.color = black
+				uncle.color = black
+				grandparent.color = red
+				newNode = grandparent
+			} else {
+				if newNode == parent.left {
+					t.rightRotate(parent)
+					newNode = parent
+				}
+				t.leftRotate(grandparent)
+				grandparent.color = red
+				newNode.parent.color = black
+			}
+		}
+	}
+	t.root.color = black
+}
